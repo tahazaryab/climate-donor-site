7a430cbd2d353e7b70646e14b47ee36263955977
huayu-ouyang <ouyang.huayu@gmail.com>
2021-02-05 15:11:04 -0800

    basic contact page

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function Contact() {
+  return (
+  	<>
+  	<h1>Contact</h1>
+  	<h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+     </>
+    )
+}
