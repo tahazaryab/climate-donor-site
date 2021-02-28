3dce639ff54a077cb81ab9212965ddf3f3946418
huayu-ouyang <ouyang.huayu@gmail.com>
2021-02-27 16:01:52 -0800

    very basic signin/authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import initAuth from '../utils/initAuth'
 
+initAuth()
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
