f7c91ff6291dfa7a8425b643ffeef3bb1d24f292
Naama <0naama0@gmail.com>
2021-06-01 15:37:59 -0700

    Add all projects to recommended tab in dashboard

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { Menu } from 'antd';
 import styles from '../styles/Sidebar.module.css'
+import Link from 'next/link'
 const { SubMenu } = Menu;
 const Sidebar = () => {
     return (
@@ -16,7 +17,7 @@ const Sidebar = () => {
                 <SubMenu key="sub1" title="Donor" className={styles.customSubmenu}>
                     <Menu.Item key="1">My Projects</Menu.Item>
                     <Menu.Item key="2">Saved Projects</Menu.Item>
-                    <Menu.Item key="3">Recommended</Menu.Item>
+                    <Menu.Item key="3"><Link href="/recommended"><a>Recommended</a></Link></Menu.Item>
                     <Menu.Item key="4">Donation Histroy</Menu.Item>
                 </SubMenu>
             </Menu>
