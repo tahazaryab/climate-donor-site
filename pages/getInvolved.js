2683bc067ac86ce4bba33ea89bf18ac41dbde264
William Wang <swang45@uw.edu>
2022-03-18 00:39:46 -0700

    Finnished Donate Card

--- pages/getInvolved.js
+++ pages/getInvolved.js
@@ -3,6 +3,7 @@ import NavBar from "../components/NavBar";
 import React from "react";
 import Hero from "../components/Hero";
 import HorizonCard from "../components/HorizonCard";
+import DonateCard from "../components/getInvolvedComponents/donateCard/DonateCard";
 
 export default function GetInvolved() {
   return (
@@ -11,10 +12,26 @@ export default function GetInvolved() {
       <Hero
         h1="Get Involved"
         h2="Engage in the fight against climate change and species extinction through projects of your choosing.  Your contribution to current projects in need of funding will make a difference."
-        type="getInvolved">
-      </Hero>
+        type="getInvolved"></Hero>
       <section className="centerContainer container grey-bg">
         <h1 className="global-h1">Donate</h1>
+        <div className="cardContainer">
+          <DonateCard img="individual" title="Individuals" content="For an individual who would like to donate to our operating budget."/>
+          <DonateCard img="foundation" title="Foundations" content="For a foundation, family office, or individual philanthropist looking to address climate change, clean energy access, and energy equity."/>
+          <DonateCard img="corporation" title="Corporations" content="For a corporation that wants to donate funds, in-kind products or services, and/or employee volunteers."/>
+        </div>
+        <Link href="/projects">
+          <div className="btn">See available projects</div>
+        </Link>
+      </section>
+      <section className="centerContainer grey-bg">
+        <h1 className="global-h1">Citizen Scientists</h1>
+        <p className="global-p">
+          Grassroots citizen involvement in helping to find and implement
+          solutions to climate change and species extinction will be critical to
+          avoid catastrophic consequences. Help us to build a critical mass of
+          Citizen Scientists by signing up now for a future study or experiment.
+        </p>
         <Link href="/about">
           <div className="btn">Learn more</div>
         </Link>
@@ -41,18 +58,6 @@ export default function GetInvolved() {
           type="contribute"
           img="volunteer.png"
           content={`For an individual volunteer who wants to contribute their time and talents to support.`}></HorizonCard>
-      </section>
-      <section className="centerContainer">
-        <h1 className="global-h1">Citizen Scientists</h1>
-        <p className="global-p">
-          Grassroots citizen involvement in helping to find and implement
-          solutions to climate change and species extinction will be critical to
-          avoid catastrophic consequences. Help us to build a critical mass of
-          Citizen Scientists by signing up now for a future study or experiment.
-        </p>
-        <Link href="/about">
-          <div className="btn">Learn more</div>
-        </Link>
       </section>
     </>
   );
