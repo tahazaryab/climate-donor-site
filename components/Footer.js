69ae8d03406200377d84d9954382625ee1faafe9
shjtia@uwaterloo.ca <shjtia@uwaterloo.ca>
2022-03-20 21:50:13 -0400

    responsive app footer created

--- components/Footer.js
+++ components/Footer.js
@@ -1,24 +1,80 @@
-import React from 'react'
-import {Component} from 'react'
-import {Layout, Menu, Dropdown} from 'antd';
+import React from "react";
+import { Component } from "react";
+import { Layout, Menu, Dropdown } from "antd";
 import styles from "../styles/Footer.module.css";
-
-
-const Footer = Layout.Footer;
+import Link from "next/link";
 
 class AppFooter extends Component {
-    render() {
-        return (
-            <Footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
+  render() {
+    return (
+      <>
+        <hr />
+        <div className={styles.footer}>
+          <div class={styles.first}>
+            <div className={styles.siteLogo}>
+              <img src="/logo2.png" alt="logo of Climate Donor" />
+              <p>Climate Donor</p>
+            </div>
+            <h2 className="subtitle" style={{fontSize: "1.5em", marginBottom: "5em", marginTop: "1em"}}>
+                ClimateDonor.org is a 501(c)(3) nonprofit organization that is
+                focused on mitigation and reversal of global climate change and
+                species extinction.
+              </h2>
+          </div>
+          <div class={styles.second}>
+          <Link href="/about">
+            <h2 className={styles.link}>About Climate Donor</h2>
+            </Link>
+          </div>
+          <div class={styles.third}>
+          <Link href="/getInvolved">
+            <h2 className={styles.link}>Get Involved</h2>
+            </Link>
+          </div>
+          <div class={styles.fourth}>
+            <h2 className={styles.title}>Connect</h2>
+            <div className={styles.media}>
+              <a
+                href="https://twitter.com/ClimateDonor/"
+                target="_blank"
+                style={{ display: "flex" }}
+              >
+                <img src="./img/icons/twitter.svg" alt="twitter"></img>
+                <span
+                  style={{ color: "black", margin: "auto", marginLeft: "1vw" }}
                 >
-                    Follow us on Social Media {' '}
-                </a>
-            </Footer>
-        );
-    }
+                  Twitter
+                </span>
+              </a>
+              <a
+                href="https://www.instagram.com/climatedonor/"
+                target="_blank"
+                style={{ display: "flex" }}
+              >
+                <img src="./img/icons/instagram.svg" alt="instagram"></img>
+                <span
+                  style={{ color: "black", margin: "auto", marginLeft: "1vw" }}
+                >
+                  Instagram
+                </span>
+              </a>
+              <a
+                href="https://www.linkedin.com/company/climatedonor-org/"
+                target="_blank"
+                style={{ display: "flex" }}
+              >
+                <img src="./img/icons/linkedin.svg" alt="linkedin"></img>
+                <span
+                  style={{ color: "black", margin: "auto", marginLeft: "1vw" }}
+                >
+                  LinkedIn
+                </span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 export default AppFooter;
