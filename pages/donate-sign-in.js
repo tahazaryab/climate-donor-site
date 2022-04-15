a19d87f14e17244cc444d40d5b4b9df6540be4ba
Glenn Chen <glenn.jun.chen@gmail.com>
2022-04-14 22:44:39 -0700

    fixed donate-sign-in.js indentation

--- pages/donate-sign-in.js
+++ pages/donate-sign-in.js
@@ -12,43 +12,43 @@ export default function donateSignIn() {
 	};
   return (
 		<>
-    	<NavBar />
+			<NavBar />
 			<br />
 			<div className={styles.mainContainer}>
-      	<h1>How would you like to donate today?</h1>
+	  		<h1>How would you like to donate today?</h1>
 				<div className={styles.gridContainer}>
 					<div className={styles.leftCol}>
 						<h3>Returning User</h3>
 						<p>Signing in with Climate Donor allows you to keep a record of your contributions!</p>
 						<Form layout="vertical">
 							<Form.Item
-                label="Email"
-                name="email"
-                colon={false}
-                rules={[
-                  {
-                    required: true,
-                    type: "email",
-                    message: "Please input your email!",
-                  },
-                ]}
-              >
-                <Input placeholder="Your Email" />
-              </Form.Item>
+								label="Email"
+								name="email"
+								colon={false}
+								rules={[
+									{
+									required: true,
+									type: "email",
+									message: "Please input your email!",
+									},
+								]}
+							>
+								<Input placeholder="Your Email" />
+			  			</Form.Item>
 							<Form.Item
-                label="Password"
-                name="password"
-                colon={false}
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your password!",
-                  },
-                ]}
-                type="password"
-              >
-                <Input.Password placeholder="Your Password" />
-              </Form.Item>
+								label="Password"
+								name="password"
+								colon={false}
+								rules={[
+									{
+									required: true,
+									message: "Please input your password!",
+									},
+								]}
+								type="password"
+							>
+								<Input.Password placeholder="Your Password" />
+							</Form.Item>
 							<Button type="primary" className={styles.signIn}>
 								Sign in
 							</Button>
