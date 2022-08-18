b45a992a440d8f55dc8036fe6cf0f448fdca03fb
Che' Goodridge <>
2022-08-18 00:44:47 -0400

    Updated UI with selection field

--- components/AdminDashComponents/Projects.js
+++ components/AdminDashComponents/Projects.js
@@ -2,13 +2,20 @@ import { useEffect, useState } from "react";
 import styles from "../../styles/Dashboard.module.css";
 import SearchBar from "../SearchBar";
 import ProjectTabs from "../ProjectTabs";
-import { Row, Table } from "antd";
+import { Row, Table, Col, Select } from "antd";
 import { getAllProjects } from "../../lib/firebase";
 import ProjectCard from "../ProjectCard";
 import { withThemeCreator } from "@material-ui/styles";
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
+
+
+
+const { Option } = Select;
+
+
 
 const red = "#ff6262";
-const yellow = "#ffff62";
+const orange = "#F07013";
 const green = "#62ff62";
 
 function ProjectList(props) {
@@ -39,7 +46,7 @@ function ProjectList(props) {
 }
 
 // from https://stackoverflow.com/questions/46240647/react-how-to-force-a-function-component-to-render
-function useForceUpdate(){
+function useForceUpdate() {
   const [value, setValue] = useState(0); // integer state
   return () => setValue(value => value + 1); // update the state to force render
 }
@@ -59,45 +66,36 @@ function AdminProjectList(props) {
     // TODO: maybe a better way to rerender than forcing update
     let temp = data;
     temp[index].status = status;
+    console.log(status);
     setTempData(temp);
     forceUpdate();
   }
+
+  const handleChange = (value) => {
+    console.log(`selected ${value}`);
+
+  };
+
 
   const columns = [
     {
       title: 'Project Name',
       dataIndex: 'name',
       render(text, row, index) {
-        let color = red;
-        if (data[index]?.status === "pending") 
-          color = yellow;
-        else if (data[index]?.status === "approved")
-          color = green;
-        
+        // let color = red;
+        // if (data[index]?.status === "pending")
+        //   color = yellow;
+        // else if (data[index]?.status === "approved")
+        //   color = green;
+
         return {
           props: {
-            style: { background: color },
+            style: { background: 'white' },
           },
           children:
             <div>
-              {data[index].status === "pending" ? 
-                <span>
-                  <span
-                    className={styles.projectRowButton}
-                    onClick={() => changeProjectStatus(getProject(parseInt(index)), index, "approved")}
-                  >
-                    ✅
-                  </span>
-                  <span
-                    className={styles.projectRowButton}
-                    onClick={() => changeProjectStatus(getProject(parseInt(index)), index, "rejected")}
-                  >
-                    ❌
-                  </span>
-                </span> : 
-                <span />
-              }
-              <a 
+
+              <a
                 target="_blank"
                 href={`/project/${getProject(parseInt(index)).id}`}
                 className={styles.projectLink}
@@ -112,16 +110,19 @@ function AdminProjectList(props) {
       title: 'Project Owner',
       dataIndex: 'owner',
       render(text, row, index) {
-        let color = red;
-        if (data[index]?.status === "pending") 
-          color = yellow;
-        else if (data[index]?.status === "approved")
-          color = green;
+        // let color = red;
+        // if (data[index]?.status === "pending")
+        //   color = yellow;
+        // else if (data[index]?.status === "approved")
+        //   color = green;
         return {
           props: {
-            style: { background: color },
+            style: { background: 'white' },
           },
-          children: <span>{text}</span>
+          children:
+            <>
+              <span>{text}</span>
+            </>
         }
       }
     },
@@ -130,15 +131,66 @@ function AdminProjectList(props) {
       dataIndex: 'status',
       render(text, row, index) {
         let color = red;
-        if (data[index]?.status === "pending") 
-          color = yellow;
+        if (data[index]?.status === "pending")
+          color = orange;
         else if (data[index]?.status === "approved")
           color = green;
         return {
           props: {
-            style: { background: color },
+            style: { background: 'white' },
+            statusDot: { background: 'red', height: '10px', width: '10px' },
+            check: { color: 'green' }
           },
-          children: <span>{text}</span>
+          children:
+            <>
+              <Row>
+                <Col span={12}>
+                  {
+                    data[index].status === "pending" ?
+                      <Select
+                        defaultValue="pending"
+                        style={{
+                          width: 120,
+                        }}
+                        onChange={() => changeProjectStatus(getProject(parseInt(index)), index, fieldNames.value)}
+                      >
+                        <Option value="approved">Approved</Option>
+                        <Option value="rejected">Rejected</Option>
+                        <Option value="pending">Pending</Option>
+
+                      </Select>
+                      :
+                      <span />
+                  }
+                  {/* {data[index].status === "pending" ?
+                    <span>
+                      <span
+                        className={styles.projectRowButton}
+                        onClick={() => changeProjectStatus(getProject(parseInt(index)), index, "approved")}
+                      >
+                        <CheckOutlined style={{ color: 'green' }} />
+                      </span>
+                      <span
+                        className={styles.projectRowButton}
+                        onClick={() => changeProjectStatus(getProject(parseInt(index)), index, "rejected")}
+                      >
+                        <CloseOutlined style={{ color: 'red' }} />
+                      </span>
+                    </span>
+                    :
+                    <span />
+                  } */}
+                </Col>
+
+                {/* <Col span={6}>
+                  <span>{text}</span>
+                </Col> */}
+                <Col span={12}>
+                  <div style={{ backgroundColor: color, height: '8px', width: '8px', borderRadius: '20px' }}></div>
+                </Col>
+              </Row>
+
+            </>
         }
       }
     },
@@ -146,14 +198,14 @@ function AdminProjectList(props) {
       title: 'Last Action',
       dataIndex: 'last_action',
       render(text, row, index) {
-        let color = red;
-        if (data[index]?.status === "pending") 
-          color = yellow;
-        else if (data[index]?.status === "approved")
-          color = green;
+        // let color = red;
+        // if (data[index]?.status === "pending")
+        //   color = yellow;
+        // else if (data[index]?.status === "approved")
+        //   color = green;
         return {
           props: {
-            style: { background: color },
+            style: { background: 'white' },
           },
           children: <span>{text}</span>
         }
@@ -223,8 +275,8 @@ export default function Projects(props) {
           }}
         />
       </Row>
-      {props.userType === "admin" ? 
-        <AdminProjectList 
+      {props.userType === "admin" ?
+        <AdminProjectList
           projects={projects}
           getProject={getProject}
           data={tableData}
